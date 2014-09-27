08760d42d1b5833106c24f6ef450ae3d2c9412a2
Justin Cano <jcano001@ucr.edu>
2014-09-26 22:03:52 -0700

    created Note class

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
 import xml.etree.ElementTree as ET
 
+class Note:
+    def __init__(self, xmlTree):
+        self.note = {}
+        self.note['tags'] = []
+        root = xmlTree.getroot()
+        for child in root:
+            if child.tag == "tag":
+                self.note['tags'].append(child.text)
+            else:
+                self.note[child.tag] = child.text
+
+    def printNote(self):
+        print "GUID:", self.note['guid']
+        print "Created at:", self.note['created']
+        if len(self.note['tags']) > 0:
+            print "Tags:",
+            for tag in self.note['tags']:
+                print tag+',',
+            print
+        print "Content:"
+        print self.note['content']
+
 
 def main():
     if raw_input() == "CREATE":
         tree = ET.parse('note0.xml')
-        root = tree.getroot()
+        note = Note(tree)
+        note.printNote()
 
 
 while __name__ == "__main__":
