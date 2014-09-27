53492c062d5734ba4391addbd613d35d125208e1
Justin Cano <jcano001@ucr.edu>
2014-09-27 00:03:53 -0700

    using xml.dom.minidom to parse string xml's

--- main.py
+++ main.py
@@ -1,34 +1,41 @@
-import xml.etree.ElementTree as ET
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+import xml.dom.minidom
+
+searchableNotes = []
 
 class Note:
-    def __init__(self, xmlTree):
+    def __init__(self, xmlDom):
         self.note = {}
-        self.note['tags'] = []
-        root = xmlTree.getroot()
-        for child in root:
-            if child.tag == "tag":
-                self.note['tags'].append(child.text)
-            else:
-                self.note[child.tag] = child.text
+        self.note['guid'] = self.getData(xmlDom.getElementsByTagName('guid')[0].childNodes)
+        self.note['created'] = self.getData(xmlDom.getElementsByTagName('created')[0].childNodes)
+        self.note['tags'] = self.getData(xmlDom.getElementsByTagName('tag')[0].childNodes, True)
+        self.note['content'] = self.getData(xmlDom.getElementsByTagName('content')[0].childNodes)
+        
+    def getData(self, nodelist, tags=False):
+        rc = []
+        for node in nodelist:
+            #if node.nodeType == node.TEXT_NODE:
+            rc.append(node.data)
+        if tags:
+            return rc
+        return ''.join(rc)
+        
 
     def printNote(self):
-        print "GUID:", self.note['guid']
-        print "Created at:", self.note['created']
-        if len(self.note['tags']) > 0:
-            print "Tags:",
-            for tag in self.note['tags']:
-                print tag+',',
-            print
-        print "Content:"
-        print self.note['content']
-
+        for key in self.note.keys():
+            print key, self.note[key]
 
 def main():
     if raw_input() == "CREATE":
-        tree = ET.parse('note0.xml')
-        note = Note(tree)
+        line = ''
+        xmlDom = ''
+        while line != "</note>":
+            line = raw_input()
+            xmlDom += line
+        dom = xml.dom.minidom.parseString(xmlDom)
+        note = Note(dom)
         note.printNote()
 
 
-while __name__ == "__main__":
-    main()
+#while __name__ == "__main__":
+main()
