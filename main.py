44fe591e726fdb77ddce41cd74c096a00646fa04
Justin Cano <jcano001@ucr.edu>
2014-09-28 00:08:05 -0700

    handles tags and created queries. needs to handle combined wildcard queries

--- main.py
+++ main.py
@@ -65,21 +65,46 @@ def delete(key):
 
 def search(query):
     queries = query.split(' ')
+    print queries
     results = []
     for note in searchableNotes:
         addNode = False
         for q in queries:
             regex = compileRegex(q)
-            if re.search(regex, note.content):
-                addNote = True
+            if "tag:" in q:
+                for tag in note.tags:
+                    if re.search(regex, tag):
+                        addNote = True
+                    else:
+                        addNote = False
+            elif "created:" in q:
+                date = regex
+                if note.created >= date:
+                    addNote = True
+                else:
+                    addNote = False
             else:
-                addNote = False
+                if re.search(regex, note.content):
+                    addNote = True
+                else:
+                    addNote = False
         if addNote:
             results.append(note.guid)
     return results
 
 def compileRegex(query):
-    return re.compile(r'\b%s\b' % query, re.IGNORECASE)
+    if "created:" not in query:
+        if "tag:" in query:
+            r = ''.join(filter(None,query.split('tag:')))
+        else:
+            r = query
+        if "*" in r:
+            r = r.replace("*", ".*")
+        regex = re.compile(r'\b%s\b' % r, re.IGNORECASE)
+    else:
+        date = ''.join(filter(None,query.split('created:')))
+        regex = datetime.datetime.strptime(date, '%Y%m%d')
+    return regex
 
 def main():
     while True:
