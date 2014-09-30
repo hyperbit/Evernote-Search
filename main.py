9a8422356d08592fa0417cec1d995f610c8ca20f
Justin Cano <jcano001@ucr.edu>
2014-09-30 00:06:25 -0700

    corrected regex to check for invalid apostrophes

--- main.py
+++ main.py
@@ -107,18 +107,9 @@ def compileRegex(query):
             r = query
         if len(r) == 0:
             return ''
-        if r == '*':
-            r = r.replace('*', '[*]')
-        elif '*' in r:
+        if r != '*' and '*' in r:
             r = r.replace('*', '.*')
-        if "'" == r[0] and "'" == r[len(r)-1]:
-            regex = re.compile(r'%s' % r, re.IGNORECASE)
-        elif "'" == r[0]:
-            regex = re.compile(r'%s\b' % r, re.IGNORECASE)
-        elif "'" == r[len(r)-1]:
-            regex = re.compile(r'\b%s' % r, re.IGNORECASE)
-        else:
-            regex = re.compile(r'\b%s\b' % r, re.IGNORECASE)
+        regex = re.compile(r'(?!\')\b%s\b(?!\')' % r, re.IGNORECASE)
     else:
         date = ''.join(filter(None,query.split('created:')))
         regex = datetime.datetime.strptime(date, '%Y%m%d')
